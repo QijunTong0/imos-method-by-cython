6fc6a1265d63209e2b81c908e30c809c66bd8b1c
QijunTong <40225103+QijunTong0@users.noreply.github.com>
2023-12-18 00:06:17 +0900

    contigious

--- stats.py
+++ stats.py
@@ -3,19 +3,19 @@ import time
 from tqdm import tqdm
 from imos_cython import imos_cython
 
-shape = np.array([200, 30, 70, 16], dtype=np.int16)
+t = 16
+shape = np.array([200, 30, 70, t], dtype=np.int16)
 n_index, d_index, _ = np.where(np.ones(shape=np.array([200, 30, 70]), dtype=np.int16))
 
 st = np.random.randint(0, shape[2] // 2, size=shape[:3], dtype=np.int16)
 ed = st + np.random.randint(0, shape[2] // 2, size=shape[:3], dtype=np.int16)
-staff_skill = np.random.randint(0, 2, size=(70, 16), dtype=np.int16)
-staff_skill_bool = np.random.randint(0, 2, size=(70, 16), dtype=np.int16)
+staff_skill = np.random.randint(0, 2, size=(70, t), dtype=np.int16)
+staff_skill_bool = np.random.randint(0, 2, size=(70, t), dtype=np.int16)
 
-arr = np.random.randint(0, 2, size=(200, 30, 70, 70, 1), dtype=np.bool_)
+arr = np.random.randint(0, 2, size=(200, 30, 70, 70), dtype=np.bool_)
 time_st = time.time()
 for i in tqdm(range(10)):
-    arr = arr * staff_skill_bool[np.newaxis, np.newaxis, :, np.newaxis, :]
-    arr.sum(axis=2)
+    np.dot(arr, staff_skill_bool)
 print("naive:", (time.time() - time_st), "s")
 
 """
