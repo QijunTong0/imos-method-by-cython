325f821fefb6765d6995363cf93ff70da6c731f7
Qijun Tong <stagcutlery@gmail.com>
2023-12-17 21:00:22 +0900

    hikitugi

--- stats.py
+++ stats.py
@@ -11,20 +11,21 @@ ed = st + np.random.randint(0, shape[2] // 2, size=shape, dtype=np.int32)
 
 arr = np.random.randint(0, 2, size=(200, 30, 70, 70), dtype=np.bool_)
 time_st = time.time()
-for i in tqdm(range(1000)):
+for i in tqdm(range(100)):
     arr.sum(axis=2)
 print("naive:", (time.time() - time_st), "s")
 
-
+"""
 st_ravel = np.ravel(st)
 ed_ravel = np.ravel(ed)
 time_st = time.time()
-for i in tqdm(range(1000)):
+for i in tqdm(range(100)):
     res = np.zeros(shape, dtype=np.int32)
     np.add.at(res, (n_index, d_index, st_ravel), 1)
     np.add.at(res, (n_index, d_index, ed_ravel), -1)
     np.cumsum(res, out=res, axis=2)
 print("imos_numpy:", (time.time() - time_st), "s")
+"""
 
 time_st = time.time()
 for i in tqdm(range(1000)):
