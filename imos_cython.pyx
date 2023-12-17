f1507bbd1ae1948ed06b1a71d9bab14077971737
QijunTong <40225103+QijunTong0@users.noreply.github.com>
2023-12-17 21:11:58 +0900

    int32->int16

--- imos_cython.pyx
+++ imos_cython.pyx
@@ -5,8 +5,8 @@ cnp.import_array()
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
-def imos_cython(cnp.ndarray[cnp.int32_t, ndim=1] shape, cnp.ndarray[cnp.int32_t, ndim=3] st, cnp.ndarray[cnp.int32_t, ndim=3] ed):
-    cdef cnp.ndarray[cnp.int32_t, ndim=3] res = np.zeros(shape,dtype=np.int32)
+def imos_cython(cnp.ndarray[cnp.int16_t, ndim=1] shape, cnp.ndarray[cnp.int16_t, ndim=3] st, cnp.ndarray[cnp.int16_t, ndim=3] ed):
+    cdef cnp.ndarray[cnp.int16_t, ndim=3] res = np.zeros(shape,dtype=np.int16)
     cdef int i,j,k
     for i in range(shape[0]):
         for j in range(shape[1]):
@@ -17,19 +17,4 @@ def imos_cython(cnp.ndarray[cnp.int32_t, ndim=1] shape, cnp.ndarray[cnp.int32_t,
         for j in range(shape[1]):
             for k in range(1,shape[2]):
                 res[i,j,k] += res[i,j,k - 1]
-    return res
-
-def imos1d_cython(
-        int n,
-        cnp.ndarray[cnp.int32_t, ndim=1] st,
-        cnp.ndarray[cnp.int32_t, ndim=1] ed,
-        ):
-    cdef cnp.ndarray[cnp.int32_t, ndim=1] res = np.zeros(n,dtype=np.int32)
-    cdef int i
-    for i in range(st.shape[0],nogil=True):
-        res[st[i]] += 1
-        res[ed[i]] -= 1
-
-    for i in range(n-1):
-        res[i+1] += res[i]
     return res
--- stats.py
+++ stats.py
@@ -3,16 +3,16 @@ import time
 from tqdm import tqdm
 from imos_cython import imos_cython
 
-shape = np.array([200, 30, 70], dtype=np.int32)
-n_index, d_index, _ = np.where(np.ones(shape=np.array([200, 30, 70]), dtype=np.int32))
+shape = np.array([200, 30, 70], dtype=np.int16)
+n_index, d_index, _ = np.where(np.ones(shape=np.array([200, 30, 70]), dtype=np.int16))
 
-st = np.random.randint(0, shape[2] // 2, size=shape, dtype=np.int32)
-ed = st + np.random.randint(0, shape[2] // 2, size=shape, dtype=np.int32)
+st = np.random.randint(0, shape[2] // 2, size=shape, dtype=np.int16)
+ed = st + np.random.randint(0, shape[2] // 2, size=shape, dtype=np.int16)
 
 arr = np.random.randint(0, 2, size=(200, 30, 70, 70), dtype=np.bool_)
 time_st = time.time()
 for i in tqdm(range(100)):
-    arr.sum(axis=2)
+    arr.sum(axis=2, dtype=np.int16)
 print("naive:", (time.time() - time_st), "s")
 
 """
@@ -20,7 +20,7 @@ st_ravel = np.ravel(st)
 ed_ravel = np.ravel(ed)
 time_st = time.time()
 for i in tqdm(range(100)):
-    res = np.zeros(shape, dtype=np.int32)
+    res = np.zeros(shape, dtype=np.in16)
     np.add.at(res, (n_index, d_index, st_ravel), 1)
     np.add.at(res, (n_index, d_index, ed_ravel), -1)
     np.cumsum(res, out=res, axis=2)
