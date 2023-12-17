b2880ae93384b09c0ac994cb41bae4d92253a378
QijunTong <40225103+QijunTong0@users.noreply.github.com>
2023-12-17 22:27:39 +0900

    対応

--- imos_cython.pyx
+++ imos_cython.pyx
@@ -6,12 +6,12 @@ cnp.import_array()
 @cython.wraparound(False)
 @cython.boundscheck(False)
 def imos_cython(
-        cnp.ndarray[cnp.int32_t, ndim=1] shape,
-        cnp.ndarray[cnp.int32_t, ndim=3] st,
-        cnp.ndarray[cnp.int32_t, ndim=3] ed,
-        cnp.ndarray[cnp.int32_t, ndim=2] staff_skill,
+        cnp.int16_t[:] shape,
+        cnp.int16_t[:,:,:] st,
+        cnp.int16_t[:,:,:] ed,
+        cnp.int16_t[:,:] staff_skill,
         ):
-    cdef cnp.ndarray[cnp.int32_t, ndim=4] res = np.zeros(shape,dtype=np.int32)
+    cdef cnp.int16_t[:,:,:,:] res = np.zeros(shape,dtype=np.int16)
     cdef int i,j,k
     for i in range(shape[0]):
         for j in range(shape[1]):
@@ -22,20 +22,6 @@ def imos_cython(
     for i in range(shape[0]):
         for j in range(shape[1]):
             for k in range(1,shape[2]):
-                res[i,j,k] += res[i,j,k - 1]
-    return res
-
-def imos1d_cython(
-        int n,
-        cnp.ndarray[cnp.int32_t, ndim=1] st,
-        cnp.ndarray[cnp.int32_t, ndim=1] ed,
-        ):
-    cdef cnp.ndarray[cnp.int32_t, ndim=1] res = np.zeros(n,dtype=np.int32)
-    cdef int i
-    for i in range(st.shape[0],nogil=True):
-        res[st[i]] += 1
-        res[ed[i]] -= 1
-
-    for i in range(n-1):
-        res[i+1] += res[i]
+                for l in range(staff_skill.shape[1]):
+                    res[i,j,k,l] += res[i,j,k - 1,l]
     return res
--- stats.py
+++ stats.py
@@ -3,15 +3,18 @@ import time
 from tqdm import tqdm
 from imos_cython import imos_cython
 
-shape = np.array([200, 30, 70], dtype=np.int32)
-n_index, d_index, _ = np.where(np.ones(shape=np.array([200, 30, 70]), dtype=np.int32))
+shape = np.array([200, 30, 70], dtype=np.int16)
+n_index, d_index, _ = np.where(np.ones(shape=np.array([200, 30, 70]), dtype=np.int16))
 
-st = np.random.randint(0, shape[2] // 2, size=shape, dtype=np.int32)
-ed = st + np.random.randint(0, shape[2] // 2, size=shape, dtype=np.int32)
+st = np.random.randint(0, shape[2] // 2, size=shape, dtype=np.int16)
+ed = st + np.random.randint(0, shape[2] // 2, size=shape, dtype=np.int16)
+staff_skill = np.random.randint(0, 2, size=(70, 10), dtype=np.int16)
+staff_skill_bool = np.random.randint(0, 2, size=(70, 10), dtype=np.int16)
 
 arr = np.random.randint(0, 2, size=(200, 30, 70, 70), dtype=np.bool_)
 time_st = time.time()
 for i in tqdm(range(100)):
+    arr = arr * staff_skill_bool[np.newaxis, np.newaxis, :, np.newaxis, :]
     arr.sum(axis=2)
 print("naive:", (time.time() - time_st), "s")
 
@@ -29,5 +32,5 @@ print("imos_numpy:", (time.time() - time_st), "s")
 
 time_st = time.time()
 for i in tqdm(range(1000)):
-    imos_cython(shape, st, ed)
+    imos_cython(shape, st, ed, staff_skill)
 print("imos_cython:", (time.time() - time_st), "s")
