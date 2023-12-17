06738760ca902d1aa09f3a937958de07249e23d7
Qijun Tong <stagcutlery@gmail.com>
2023-12-17 19:18:51 +0900

    test

--- imos_cython.pyx
+++ imos_cython.pyx
@@ -7,13 +7,12 @@ cnp.import_array()
 @cython.boundscheck(False)
 def imos_cython(cnp.ndarray[cnp.int32_t, ndim=1] shape, cnp.ndarray[cnp.int32_t, ndim=3] st, cnp.ndarray[cnp.int32_t, ndim=3] ed):
     cdef cnp.ndarray[cnp.int32_t, ndim=3] res = np.zeros(shape,dtype=np.int32)
-    cdef int i,j,k,temp
+    cdef int i,j,k
     for i in range(shape[0]):
         for j in range(shape[1]):
             for k in range(st.shape[2]):
-                # temp=st[i,j,k]
                 res[i,j,st[i,j,k]] += 1
-                res[i,j,ed[i,j,k]]-= 1
+                res[i,j,ed[i,j,k]] -= 1
     for i in range(shape[0]):
         for j in range(shape[1]):
             for k in range(1,shape[2]):
